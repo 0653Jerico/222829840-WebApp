54b807a8f5988662ac6bf68cd8eaad86126c9385
0653Jerico <markuzkrei432@gmail.com>
2024-02-08 17:40:08 +0800

    Fixed the form html and css, added a simple alertbox to js

--- Assets/script/html3_script.js
+++ Assets/script/html3_script.js
@@ -3,3 +3,7 @@ const userAge = document.getElementById("age");
 const userAddress = document.getElementById("address");
 
 const formSubmit = document.querySelector("#submit-button");
+
+function personalInfoSubmit(){
+    confirm("Successfully Submitted!");
+}
